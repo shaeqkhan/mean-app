babee5f4ff56ff03566dbdfd5f9efa9bb8428056
Shaeq Khan <shaeqkhan@gmail.com>
2016-12-31 14:11:46 -0500

    added post to mongodb

--- exercise3/api/controllers/hotels.controllers.js
+++ exercise3/api/controllers/hotels.controllers.js
@@ -38,21 +38,42 @@ module.exports.hotelsGetOne = function(req, res) {
 	var hotelId = req.params.hotelId;
 	console.log("GET a hotel", hotelId);
 	
-collection
-	.findOne({
-		_id : ObjectId(hotelId)
-	}, function(err, doc) {
-		res
-		.status(200)
-		.json(doc);
-	});
+	collection
+		.findOne({
+			_id : ObjectId(hotelId)
+		}, function(err, doc) {
+			res
+			.status(200)
+			.json(doc);
+		});
 	
 };
 
 module.exports.hotelsAddOne = function(req, res) {
+	
+	var db = dbconn.get();
+	var collection = db.collection('hotels');
+	var newHotel;
+
 	console.log("POST new hotel");
-	console.log(req.body);
-	res.
-		status(200)
-		.json(req.body);
+
+	if(req.body && req.body.name && req.body.stars) {
+		newHotel = req.body;
+		newHotel.stars = parseInt(req.body.stars, 10);
+		console.log(req.body);
+		collection.insertOne(newHotel, function(err, response) {
+			console.log(response.ops);
+			res
+				.status(201)
+				.json(response.ops);
+		});		
+	}
+	else {
+		console.log("Data missing from body");
+		res
+			.status(400)
+			.json({message : "Required data missing from body"});
+	}
+
+	
 };
